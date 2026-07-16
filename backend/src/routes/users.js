ec2b95d5b68d1363ff3f5d1c503689767effd4c8
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-16 11:51:50 +0800

    feat(ch6): create login route

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,4 +1,4 @@
-import { createUser } from '../services/users.js'
+import { createUser, loginUser } from '../services/users.js'
 
 export function userRoutes(app) {
   app.post('/api/v1/user/signup', async (req, res) => {
@@ -9,6 +9,18 @@ export function userRoutes(app) {
       console.error('error creating user', err)
       return res.status(400).json({
         error: 'failed to create the user, does the username already exist?',
+      })
+    }
+  })
+
+  app.post('/api/v1/user/login', async (req, res) => {
+    try {
+      const token = await loginUser(req.body)
+      return res.status(200).send({ token })
+    } catch (err) {
+      console.error(err)
+      return res.status(400).send({
+        error: 'login failed, did you enter the correct username/password?',
       })
     }
   })
