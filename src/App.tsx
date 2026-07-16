d45b5713a1a731784c13f000d2f525c4535c6211
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-16 12:33:29 +0800

    feat(ch6): use react router to implement mutiple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import Blog from './Blog'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Blog />,
+  },
+])
 
 const queryClient = new QueryClient()
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Blog />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
