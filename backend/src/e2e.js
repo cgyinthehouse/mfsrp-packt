6ca2494868e442a78f7714e831ecf9d261cc8c4a
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-22 14:27:18 +0800

    feat(ch9-e2e): prepare the backend for e2e testing

---
+++ backend/src/e2e.js
@@ -0,0 +1,15 @@
+import { configDotenv } from 'dotenv'
+configDotenv()
+import globalSetup from './test/globalSetup.js'
+import { app } from './app.js'
+import { initDatabase } from './db/init.js'
+
+async function runTestingServer() {
+  await globalSetup()
+  await initDatabase()
+  const PORT = process.env.PORT
+  app.listen(PORT)
+  console.info(`TESTING express server running on http://localhost:${PORT}`)
+}
+
+runTestingServer()
