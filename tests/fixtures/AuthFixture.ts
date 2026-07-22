0d1a74c40ca14e147bff8835758637a5eb5c0ea7
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-22 16:33:27 +0800

    feat(ch9-e2e): create custom fixture and use it

---
+++ tests/fixtures/AuthFixture.ts
@@ -0,0 +1,20 @@
+import type { Page } from '@playwright/test'
+export class AuthFixture {
+  page: Page
+  constructor(page: Page) {
+    this.page = page
+  }
+  async signUpAndLogIn() {
+    const testUser = 'test' + Date.now()
+    await this.page.goto('/signup')
+    await this.page.getByLabel('Username:').fill(testUser)
+    await this.page.getByLabel('Password:').fill('password')
+    await this.page.getByRole('button', { name: 'Sign Up' }).click()
+    await this.page.waitForURL('**/login')
+    await this.page.getByLabel('Username:').fill(testUser)
+    await this.page.getByLabel('Password:').fill('password')
+    await this.page.getByRole('button', { name: 'Log In' }).click()
+    await this.page.waitForURL('**/')
+    return testUser
+  }
+}
