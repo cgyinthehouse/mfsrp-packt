ee99b559e4835efe4ea6d64f30def10444a9c8ad
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-22 08:10:15 +0800

    feat(ch8-SEO): use OG article meta tags

--- src/pages/ViewPost.tsx
+++ src/pages/ViewPost.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 import Header from '../components/Header'
 import Post from '../components/Post'
 import { getPostById } from '../api/posts'
+import { getUserInfo } from '../api/users'
 
 function truncate(str: string, max: number = 160) {
   if (!str) return str
@@ -19,12 +20,27 @@ export function ViewPost({ postId }: { postId: string }) {
     queryFn: () => getPostById(postId),
   })
   const post = postQuery.data
+
+  const userInfoQuery = useQuery({
+    queryKey: ['user', post?.author],
+    queryFn: () => getUserInfo(postId),
+    enabled: Boolean(post?.author),
+  })
+  const userInfo = userInfoQuery.data ?? {}
   return (
     <div style={{ padding: 8 }}>
       {post && (
         <Helmet>
           <title>{`${post.title} | Full-Stack React Blog`}</title>
           <meta name='description' content={truncate(post.contents)} />
+          <meta property='og:type' content='article' />
+          <meta property='og:title' content={post.title} />
+          <meta property='og:article:published_time' content={post.createdAt} />
+          <meta property='og:article:modified_time' content={post.updatedAt} />
+          <meta property='og:article:author' content={userInfo.username} />
+          {(post.tags ?? []).map((tag: string) => (
+            <meta property='og:article:tag' content={tag} key={tag} />
+          ))}
         </Helmet>
       )}
       <Header />
